a40a5cba2502fde68fc508607bf68fefa8dc2411
Timothy Ganoza <jostimian@gmail.com>
2020-07-09 17:28:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Covid19PH
+This is a Covid19 tracker exclusively for Philipines **[Global](https://github.com/jostimian/Covid19-project-)** is also available
+# Installation
+1. Download this repository and install the required libraries **You need[ Chrome](https://www.google.com/intl/en_ph/chrome/) to properly run this code**
+
+```bash
+pip install selenium
+pip install colorama
+```
+or you can use the requirements.txt
+```bash
+pip install -r requirements.txt
+```
+# How To Use
+Double click Main.py and type 
+```bash
+/help
+``` 
+to see all existing commands
---
+++ main.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from colorama import Fore, Back, Style
+from colorama import init
+from selenium.webdriver.chrome.options import Options
+init(autoreset=True)
+from time import sleep
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
+start_url = "https://news.google.com/covid19/map?hl=en-PH&mid=/m/05v8c&gl=PH&ceid=PH:en"
+driver.get(start_url)
+loop = True
+print(Style.BRIGHT + Fore.GREEN + "CSP ERROR MIGHT APPEAR JUST IGNORORE")
+while loop == True:
+    def latest():
+
+        latest_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[1]/div[2]')
+        latest_list = latest_xpath.text
+        return latest_list 
+    def recovered():
+
+        recovered_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[2]/div[2]')
+        recovered_list =  recovered_xpath.text
+        return recovered_list
+    def deaths():
+
+        deaths_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[3]/div[2]')
+        deaths_list =  deaths_xpath.text
+        return deaths_list 
+    def seeall():
+        latest()
+        latest_list = latest()
+        sleep(0.2)
+        recovered()
+        recovered_list = recovered()
+        sleep(0.3)
+        deaths()
+        deaths_list = deaths()
+        print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)
+        print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
+        print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)
+
+    for i in range (2):
+        print("\n")
+    user_input = input("use /help to see all of the commands: ")
+    if user_input == "/help":
+        print(Fore.CYAN + Style.BRIGHT + """
+        /all = view all confirmed cases recoveries and deaths
+        /deaths = view all confirmed deaths
+        /recover = view all confirmed recoveries
+        /cases = view all confirmed cases
+        /quit = quit the aplication
+        Save To Excel Is Still In Development
+        """)
+    if user_input == "/deaths":
+        deaths_list = deaths()
+        print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)
+    if user_input == "/recover":
+        recovered_list = recovered()
+        print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)
+    if user_input == "/cases":
+        latest_list = latest()
+        print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
+    if user_input == "/all":
+        seeall()
+    if user_input == "/quit":
+        driver.quit()
+        loop = False
