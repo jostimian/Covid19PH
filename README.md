518d7327089308dc01a66189010f2f8c697bb8fd
Timothy Ganoza <jostimian@gmail.com>
2020-07-10 14:02:04 +0800

    New DataBase And Faster Load Time

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Covid19PH
 This is a Covid19 tracker exclusively for Philipines **[Global](https://github.com/jostimian/Covid19-project-)** is also available
+# Whats new?
+new database
+faster loadtime
+**old version is still available**
 # Installation
 1. Download this repository and install the required libraries **You need[ Chrome](https://www.google.com/intl/en_ph/chrome/) to properly run this code**
 
@@ -15,10 +19,7 @@ pip install -r requirements.txt
 Double click Main.py
 if you see this error just ignore. it is the content security polices
 ```bash
-[0709/194655.636:INFO:CONSOLE(524)] "%c%s color: red; background: yellow; font-size: 24px; WARNING!", source: /_/mss/boq-dots/_/js/k=boq-dots.DotsSplashUi.en_GB.A6P3omu8MmY.es5.O/am=CqE7HmQ/d=1/excm=_b,_tp,covidview/ed=1/dg=0/wt=2/ct=zgms/rs=ALs0n2MTqn6JWEU3BNHq8E_JpxdEPaVGLg/m=_b,_tp (524)
-[0709/194655.637:INFO:CONSOLE(524)] "%c%s font-size: 18px; Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.
-Do not enter or paste code that you don't understand.", source: /_/mss/boq-dots/_/js/k=boq-dots.DotsSplashUi.en_GB.A6P3omu8MmY.es5.O/am=CqE7HmQ/d=1/excm=_b,_tp,covidview/ed=1/dg=0/wt=2/ct=zgms/rs=ALs0n2MTqn6JWEU3BNHq8E_JpxdEPaVGLg/m=_b,_tp (524)
-[0709/194656.543:INFO:CONSOLE(0)] "An <img> element was lazyloaded with loading=lazy, but had no dimensions specified. Specifying dimensions improves performance. See https://crbug.com/954323", source: https://news.google.com/covid19/map?hl=en-PH&mid=/m/05v8c&gl=PH&ceid=PH:en (0) 
+[0710/140007.021:INFO:CONSOLE(0)] "A cookie associated with a cross-site resource at https://paypal.com/ was set without the `SameSite` attribute. A future release of Chrome will only deliver cookies with cross-site requests if they are set with `SameSite=None` and `Secure`. You can review cookies in developer tools under Application>Storage>Cookies and see more details at https://www.chromestatus.com/feature/5088147346030592 and https://www.chromestatus.com/feature/5633521622188032.", source: https://covid19ph.com/ (0) 
 ```
 to see all exsiting command type
 ```bash
@@ -26,9 +27,3 @@ to see all exsiting command type
 ``` 
 # Others
 if you fond some issue please file an issue and i will try to fix it as soon as posible 
-
-# Beta Version
-the beta version is still full of bugs and extremely slow.
-
-## what is changed? 
-We added Active Cases. We also use a more up to date database
--- old.py
+++ old.py
@@ -6,27 +6,23 @@ init(autoreset=True)
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
-start_url = "http://www.covid19.gov.ph/"
+start_url = "https://news.google.com/covid19/map?hl=en-PH&mid=/m/05v8c&gl=PH&ceid=PH:en"
 driver.get(start_url)
 loop = True
-print(Style.BRIGHT + Fore.GREEN + "CSP ERROR MIGHT APPEAR JUST IGNORORE")
+print(Style.BRIGHT + Fore.GREEN + "Content Security Policy(CSP) ERROR MIGHT APPEAR JUST IGNORORE")
 while loop == True:
     def latest():
-        latest_xpath = driver.find_element_by_xpath('//*[@id="post-17"]/div/div/div/div/section[1]/div/div/div[2]/div/div/div[3]/div/div/div[1]/span[2]')
+        latest_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[1]/div[2]')
         latest_list = latest_xpath.text
         return latest_list 
     def recovered():
-        recovered_xpath = driver.find_element_by_xpath('//*[@id="post-17"]/div/div/div/div/section[1]/div/div/div[2]/div/div/div[4]/div/div/div[1]/span[2]')
+        recovered_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[2]/div[2]')
         recovered_list =  recovered_xpath.text
         return recovered_list
     def deaths():
-        deaths_xpath = driver.find_element_by_xpath('//*[@id="post-17"]/div/div/div/div/section[1]/div/div/div[2]/div/div/div[5]/div/div/div[1]/span[2]')
+        deaths_xpath = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[3]/div[2]')
         deaths_list =  deaths_xpath.text
         return deaths_list 
-    def active():
-        active_xpath = driver.find_element_by_xpath('//*[@id="post-17"]/div/div/div/div/section[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/span[2]')
-        active_list = active_xpath.text
-        return active_list
     def seeall():
         latest()
         recovered()
@@ -34,12 +30,10 @@ while loop == True:
         print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)
         print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
         print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)
-        print(Style.BRIGHT + Back.LIGHTRED_EX + "Active Cases", active_list)
     latest_list = latest()
     recovered_list = recovered()
     deaths_list = deaths()
-    active_list = active()
-    for i in range (2):
+    for new_line in range (2):
         print("\n")
     user_input = input("use /help to see all of the commands: ")
     if user_input == "/help":
@@ -48,18 +42,15 @@ while loop == True:
         /deaths = view all confirmed deaths
         /recover = view all confirmed recoveries
         /cases = view all confirmed cases
-        /active = view all active cases
         /quit = quit the aplication
         """)
-        print(Back.MAGENTA + "We I mean I already change the database")
+        print(Back.MAGENTA + "We I mean I am planing to change database to make the numbers more accurate")
     if user_input == "/deaths":
         print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)
     if user_input == "/recover":
         print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)
     if user_input == "/cases":
         print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
-    if user_input == "/active":
-        print(Style.BRIGHT + Back.LIGHTRED_EX + "Active Cases", active_list)
     if user_input == "/all":
         seeall()
     if user_input == "/quit":
