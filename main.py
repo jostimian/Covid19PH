3f0482898d68f86e548e13c22c05384cd241583c
Timothy Ganoza <jostimian@gmail.com>
2020-07-11 15:54:23 +0800

    Shorter Code

--- main.py
+++ main.py
@@ -18,39 +18,39 @@ loop = True
 
 while loop == True:
 
-    def latest():
+    def latest():#get latest cases
         latest_xpath = driver.find_element_by_xpath('//*[@id="mainapp"]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div/h3')
         latest_list = latest_xpath.text
         return latest_list 
         
-    def recovered():
+    def recovered():# get recovered cases
         recovered_xpath = driver.find_element_by_xpath('//*[@id="mainapp"]/div[2]/div[1]/div[5]/div/div/div[1]/div[2]/div/h3')
         recovered_list =  recovered_xpath.text
         return recovered_list
 
-    def deaths():
+    def deaths():# get all deaths
         deaths_xpath = driver.find_element_by_xpath('//*[@id="mainapp"]/div[2]/div[1]/div[4]/div/div/div[1]/div[2]/div/h3')
         deaths_list =  deaths_xpath.text
         return deaths_list 
 
-    def active():
+    def active():# get all active cases
         active_xpath = driver.find_element_by_xpath('//*[@id="mainapp"]/div[2]/div[1]/div[3]/div/div/div[1]/div[2]/div/h3')
         active_list = active_xpath.text
         return active_list
 
-    def seeall():
-        latest()
-        recovered()
-        deaths()
-        print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)
-        print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
-        print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)
-        print(Style.BRIGHT + Back.LIGHTYELLOW_EX + "Active Cases", active_list)
+    def seeall():# get all 
+        latest() # run latest
+        recovered()# run recovered
+        deaths()# run Deaths
+        print(Style.BRIGHT + Back.GREEN + "Recovered", recovered_list)# print recovered
+        print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)# print latest cases
+        print(Style.BRIGHT + Back.RED + "Deaths", deaths_list)# print latest death list 
+        print(Style.BRIGHT + Back.YELLOW + "Active Cases", active_list)# print all active cases
 
-    latest_list = latest()
-    recovered_list = recovered()
-    deaths_list = deaths()
-    active_list = active()
+    latest_list = latest() # latest list is inside of latest function
+    recovered_list = recovered()# recovered list is inside of recoverd function
+    deaths_list = deaths()# deaths list is inside of deaths function
+    active_list = active()# active list is inside of active function
     for i in range (2):
         print("\n")
 
@@ -76,7 +76,7 @@ while loop == True:
         print(Style.BRIGHT + Back.YELLOW + "Cases", latest_list)
 
     if user_input == "/active":
-        print(Style.BRIGHT + Back.LIGHTYELLOW_EX + "Active Cases", active_list)
+        print(Style.BRIGHT + Back.YELLOW + "Active Cases", active_list)
 
     if user_input == "/all":
         seeall()
